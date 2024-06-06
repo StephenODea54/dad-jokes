ea30956e58c195c8b5a3c31b0b1cd44b23fc887a
s.odea <s.odea@trustetc.com>
2024-06-06 13:00:11 -0400

    add dad joke generation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-let heading = document.querySelector('#heading') as HTMLHeadingElement
+const value = document.getElementById("dad-joke")
 
-heading?.classList.add('heading')
+const url = 'https://icanhazdadjoke.com/';
+const options = {
+	method: 'GET',
+	headers: {
+		'Accept': 'application/json',
+	}
+};
+
+document.getElementById("generate-button")?.addEventListener("click", e => {
+    e.preventDefault();
+
+    fetch(
+        url,
+        options
+      )
+      .then(response => response.json())
+      .then(data => {
+        if (value) {
+            value.innerHTML = data["joke"]
+        }
+      })
+      .catch(err => console.error(err))
+})
